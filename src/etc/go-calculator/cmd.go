8cf20fc2db13a250778000b318ebfdfb8b72d690
Syafdia Okta <syafdia@gmail.com>
2023-06-25 21:31:42 +0700

    Use redis on go-calculator

--- src/etc/go-calculator/cmd.go
+++ src/etc/go-calculator/cmd.go
@@ -1,45 +1,118 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
-func PingHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "OK",
-		"data":    "pong",
-	})
+type Module struct {
+	redisClient *redis.Client
 }
 
-func AddHandler(c *gin.Context) {
-	urlQuery := c.Request.URL.Query()
-	rawA := urlQuery.Get("a")
-	rawB := urlQuery.Get("b")
+func PingHandler(m *Module) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, err := m.redisClient.Ping(c).Result()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": fmt.Sprintf("Redis is not OK, err: %v", err),
+			})
+			return
+		}
 
-	a, _ := strconv.Atoi(rawA)
-	b, _ := strconv.Atoi(rawB)
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "OK",
-		"data":    a + b,
-	})
+		c.JSON(http.StatusOK, gin.H{
+			"message": "OK",
+			"data":    "pong",
+		})
+	}
 }
+
+func AddHandler(m *Module) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		urlQuery := c.Request.URL.Query()
+		rawA := urlQuery.Get("a")
+		rawB := urlQuery.Get("b")
+
+		maxNum, _ := strconv.Atoi(envMaxNumber)
+		a, _ := strconv.Atoi(rawA)
+		b, _ := strconv.Atoi(rawB)
+
+		if a > maxNum || b > maxNum {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": fmt.Sprintf("max allowed a or b value is %d", maxNum),
+			})
+			return
+		}
+
+		cacheKey := fmt.Sprintf("add:%d+%d", a, b)
+		var result int
+
+		_, err := m.redisClient.Get(c, cacheKey).Result()
+		if err != nil {
+			if !errors.Is(err, redis.Nil) {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": fmt.Sprintf("Redis is not OK when GET, err: %v", err),
+				})
+				return
+			}
+
+			result = doHeavyCalculation(a, b)
+
+			_, err := m.redisClient.Set(c, cacheKey, result, 30*time.Second).Result()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": fmt.Sprintf("Redis is not OK when SET, err: %v", err),
+				})
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "OK",
+			"data":    result,
+		})
+	}
+}
+
+func doHeavyCalculation(a int, b int) int {
+	time.Sleep(3 * time.Second)
+	return a + b
+}
+
+var (
+	envHttpServerPort = os.Getenv("HTTP_SERVER_PORT")
+	envMaxNumber      = os.Getenv("MAX_NUMBER")
+	envRedisURL       = os.Getenv("REDIS_URL")
+)
 
 func main() {
-	httpServerPort := os.Getenv("HTTP_SERVER_PORT")
-	if httpServerPort == "" {
-		httpServerPort = "8080"
+	httpServerPort := "8080"
+	if envHttpServerPort != "" {
+		httpServerPort = envHttpServerPort
+	}
+
+	redisOpt, err := redis.ParseURL(envRedisURL)
+	if err != nil {
+		log.Panicf("[main] Failed parsing Redis URL, err: %s", err)
+	}
+
+	redisClient := redis.NewClient(redisOpt)
+
+	module := &Module{
+		redisClient: redisClient,
 	}
 
 	r := gin.Default()
 
-	r.GET("/ping", PingHandler)
-	r.GET("/add", AddHandler)
+	r.GET("/ping", PingHandler(module))
+	r.GET("/add", AddHandler(module))
 
 	r.Run(fmt.Sprintf(":%s", httpServerPort))
 }
--- src/etc/go-calculator/go.mod
+++ src/etc/go-calculator/go.mod
@@ -5,6 +5,11 @@ go 1.17
 require github.com/gin-gonic/gin v1.9.0
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -19,6 +24,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/redis/go-redis/v9 v9.0.5
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
